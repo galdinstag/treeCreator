6399df2e9d5fce44d8c3624553fff50a20fe7f01
galdinstag <galdinstag@post.bgu.ac.il>
2015-09-10 11:39:44 +0300

    9.1

--- Main.java
+++ Main.java
@@ -6,9 +6,8 @@ import treeCreator.TreeCreator;
 public class Main {
     public static void main(String[] args){
        TreeCreator tree = new TreeCreator();
-        //{input file, intermidiate files folder, output file folder, R script }
+        //{input file, intermediate files folder, output file folder, R script }
         tree.CreateTree("D:/Google_Drive/workspace/treeCreator/input/samples.json","D:/Google_Drive/workspace/treeCreator/midFiles"
                 , "D:/Google_Drive/workspace/treeCreator/output","D:/Google_Drive/workspace/treeCreator/src/SeqTrakTreeCreator.R");
-        System.out.print("test");
     }
 }
